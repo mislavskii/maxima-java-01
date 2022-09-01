93945abd36db48844b88289998ef9095a3f7aab0
mislavskii <jornjat@gmail.com>
2022-09-01 23:40:22 +0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        String str = "Hello";
+
+        System.out.println( "Hello World!" );
+        System.out.println(str);
+    }
+}
