af78d760b3cd760493c29bf739496ca167418d82
mislavskii <jornjat@gmail.com>
2022-09-10 11:23:37 +0700

    subprogram arg

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,16 +2,19 @@ package org.example;
 
 public class App
 {
-    public static void main( String[] args )
-    {
-        mySubProgram();
-        mySubProgram();
+    public static void main( String[] args ) {
+        mySubProgram("Hello, World!");
+        mySubProgram("Hello, Java");
+        mySubProgram("Hello, people");
     }
 
-    public static void mySubProgram() {
+    public static void mySubProgram(String msg) {
+        System.out.println(msg);
+    }
+}
+
+/*
         System.out.println("Hello, World!");
         System.out.println("Hello, Java");
         System.out.println("Hello, people");
-        System.out.println();
-    }
-}
+ */
