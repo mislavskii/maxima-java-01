91529f1f205acbdee63179dd97b966df5aff0642
mislavskii <jornjat@gmail.com>
2022-09-11 14:13:16 +0700

    flag

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,17 +3,28 @@ package org.example;
 public class App
 {
     public static void main( String[] args ) {
-        System.out.println(factorial(4));
+        System.out.println(sayHello("Java",5, true));
     }
 
-    public static int factorial(int n) {
-        return n == 1 ? n : n * factorial(n-1);
+    public static String sayHello(String msg, int num, boolean flag) {
+        if (flag) {
+            System.out.println("Istina");
+        }
+        return "Hello " + num + " " + msg + "!!";
     }
-}
+
+
 
 /*
-        System.out.println("Hello, World!");
-        System.out.println("Hello, Java");
-        System.out.println("Hello, people");
-        System.out.println("Hello " + num + " " + msg + "!!");
- */
+        public static int factorial(int n) {
+        return n == 1 ? n : n * factorial(n-1);
+    }
+
+    public static  long fibonazzi (byte N) {
+        byte a = 0;
+        byte b = 1;
+        byte c;
+        return N == 0 ? N : a + b; a = b; b = c; N--;
+    }
+*/
+}
