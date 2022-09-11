605037a0299fb34c482f12b893e172381f3fda44
mislavskii <jornjat@gmail.com>
2022-09-11 21:01:39 +0700

    switch seasons

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,29 +4,44 @@ public class App
 {
     public static void main( String[] args ) {
         System.out.println(sayHello("Java",5, false));
+        System.out.println(getSeason(13));
     }
 
     public static String sayHello(String msg, int num, boolean flag) {
         if (flag) {
-            return "Hello " + num + " " + msg + "!!";
+            return "Привет " + num + " " + msg + "!!";
         } else {
-            return "Good bye " + num + " " + msg + "!!";
+            return "Пока " + num + " " + msg + "!!";
         }
-//        return "Hello " + num + " " + msg + "!!";
     }
 
-
-
-/*
-        public static int factorial(int n) {
-        return n == 1 ? n : n * factorial(n-1);
+    public static String getSeason (int month) {
+        String result;
+        switch (month) {
+            case 1:
+            case 2:
+            case 12:
+                result = "Winter";
+                break;
+            case 3:
+            case 4:
+            case 5:
+                result = "Spring";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                result = "Summer";
+                break;
+            case 9:
+            case 10:
+            case 11:
+                result = "Autumn";
+                break;
+            default:
+                result = "Unknown season";
+        }
+        return result;
     }
 
-    public static  long fibonazzi (byte N) {
-        byte a = 0;
-        byte b = 1;
-        byte c;
-        return N == 0 ? N : a + b; a = b; b = c; N--;
-    }
-*/
 }
