598940ec7977b4d5fb24a24753a824ac9b72ba77
mislavskii <jornjat@gmail.com>
2022-09-18 02:19:53 +0700

    daysCount

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -16,18 +16,15 @@ public class App {
 
     public static byte daysCount(byte month, int year) {
         switch(month) {
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-                return 31;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
             case 2:
                 if (isLeap(year)) {return 29;} else {return 28;}
             default:
-                return 30;
+                return 31;
         }
     }
 
